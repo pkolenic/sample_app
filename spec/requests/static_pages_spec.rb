2148c298b6c4265475a31ee87f68cc0b43aea1a4
Patrick Kolenic <pkolenic@gmail.com>
2013-07-20 20:22:04 -0600

    Add Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,5 +43,13 @@ describe "StaticPages" do
     end
     
   end
+  
+  describe "Contact page" do
+    
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 
 end
