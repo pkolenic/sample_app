f32dc6ee88b5a22a7aab8a9a62d5b64eb8978fd4
Patrick Kolenic <pkolenic@gmail.com>
2013-07-27 14:06:22 -0600

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,51 @@ describe "User pages" do
 
   subject { page }
 
+  shared_examples_for "all user pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:heading)    { user.name }
+    let(:page_title) { user.name }
+    before { visit user_path(user) }
+
+    it_should_behave_like "all user pages"
+  end
+
   describe "signup page" do
+    let(:heading)    { 'Sign up' }
+    let(:page_title) { 'Sign up' }
     before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it_should_behave_like "all user pages"
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
