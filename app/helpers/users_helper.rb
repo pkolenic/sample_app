11f6d13d5ae32b149757e956e40e4bb019e6831a
Patrick Kolenic <pkolenic@gmail.com>
2013-11-13 11:57:47 -0700

    Add Custom Avatar to Disqus comments

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,7 @@
 module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user, options = { size: 50 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    if user.clan_name == 'Fear the Fallen' || user.clan_name.blank?
-      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{request.protocol}#{request.host_with_port}#{image_path('logo.png')}"
-    else
-      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{user.clan_logo}"
-    end
-    # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{request.protocol}#{request.host_with_port}#{image_path('logo.png')}"
-    # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=https://fearthefallen.herokuapp.com/assets/logo-6dfef04d5fad85fcff9ff34f039373d2.png"
-    #link_to image_tag(gravatar_url, alt: user.name, id: 'profileImage'), 'http://gravatar.com', target: "_blank", title: "Change your avatar at Gravatar."
-    image_tag gravatar_url, alt: user.name, id: 'profileImage'
+    image_tag gravatar_url(user, options), alt: user.name, id: 'profileImage'
   end
 
   def sso_auth(user)
@@ -19,11 +9,22 @@ module UsersHelper
       digest = OpenSSL::Digest::Digest.new('sha1')
       disqus_timestamp = Time.now.to_i
       user_id = "ftf-#{user.id}#{Rails.env.development? ? '-dev' : ''}"
-      
-      disqus_serializer_message = {"id"=>user_id, "username"=>user.wot_name, "email"=>user.email}.to_json            
+           
+      # disqus_serializer_message = {"id"=>user_id, "username"=>user.wot_name, "email"=>user.email, "avatar"=>user.clan_logo}.to_json
+      disqus_serializer_message = {"id"=>user_id, "username"=>user.wot_name, "email"=>user.email, "avatar"=>gravatar_url(user)}.to_json               
       disqus_message = Base64.strict_encode64(disqus_serializer_message)
       disqus_signature = OpenSSL::HMAC.hexdigest(digest, ENV['DISQUS_SECRET'], disqus_message + ' ' + disqus_timestamp.to_s)
       auth = "#{disqus_message} #{disqus_signature} #{disqus_timestamp}"
+    end
+  end
+
+  def gravatar_url(user, options = { size: 50 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    if user.clan_name == 'Fear the Fallen' || user.clan_name.blank?
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{request.protocol}#{request.host_with_port}#{image_path('logo.png')}"
+    else
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{user.clan_logo}"
     end
   end
 
