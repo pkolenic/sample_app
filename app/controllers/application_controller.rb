44a11ac7e97ce4e6378062086cb9ed0265af53df
Patrick Kolenic <pkolenic@gmail.com>
2014-03-10 17:51:36 -0600

    Convert TS images to local copy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,15 @@ class ApplicationController < ActionController::Base
  def get_teamspeak_status
     url = "http://www.tsviewer.com/ts3viewer.php?ID=#{CLAN_TSVIEW_ID}&text=000000&text_size=12&text_family=1&js=1&text_s_weight=bold&text_s_style=normal&text_s_variant=normal&text_s_decoration=none&text_s_color_h=525284&text_s_weight_h=bold&text_s_style_h=normal&text_s_variant_h=normal&text_s_decoration_h=underline&text_i_weight=normal&text_i_style=normal&text_i_variant=normal&text_i_decoration=none&text_i_color_h=525284&text_i_weight_h=normal&text_i_style_h=normal&text_i_variant_h=normal&text_i_decoration_h=underline&text_c_weight=normal&text_c_style=normal&text_c_variant=normal&text_c_decoration=none&text_c_color_h=525284&text_c_weight_h=normal&text_c_style_h=normal&text_c_variant_h=normal&text_c_decoration_h=underline&text_u_weight=bold&text_u_style=normal&text_u_variant=normal&text_u_decoration=none&text_u_color_h=525284&text_u_weight_h=bold&text_u_style_h=normal&text_u_variant_h=normal&text_u_decoration_h=none"
     response = self.class.get url
-    @ts_status = response.parsed_response.html_safe
+    
+    raw = response.parsed_response.html_safe
+    files = Dir.glob("app/assets/images/ts/*")
+    files.each do |file|
+      file_name = file.gsub('app/assets/images/ts/', '')
+      raw.gsub! "http://static.tsviewer.com/images/teamspeak3/standard/#{file_name}", view_context.image_url("ts/#{file_name}")
+    end
+        
+    @ts_status = raw.html_safe
   end
   
 end
